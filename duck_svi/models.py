9709250453db2379c609b5af4bd3a30a968876b1
juggernut <stefanciobo@gmail.com>
2014-10-21 14:20:01 +0200

    Added compteur hard auditeur

--- duck_svi/models.py
+++ duck_svi/models.py
@@ -5,7 +5,7 @@ from django_apogee.models import Etape
 
 class WishSvi(models.Model):
     code_dossier = models.CharField(db_column='NumDossier', primary_key=True , max_length=10)
-    password = models.CharField(db_column='Password', null=False, default='1234')
+    password = models.CharField(db_column='Password', null=False, default='1234', max_length=10)
     etat = models.CharField(db_column='Etat', null=False, max_length=250)
     gestionaire_etape = models.CharField(db_column='GestModif', null=False, max_length=6)
     psycho = models.BooleanField(db_column='Psycho', null=False)
