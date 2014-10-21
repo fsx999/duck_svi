bc48679d71c2339c39ad1b4f7c10517de6d01d78
juggernut <stefanciobo@gmail.com>
2014-10-21 14:01:31 +0200

    Added compteur hard auditeur

---
+++ duck_svi/management/commands/initialisation_svi.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from django.conf import settings
+from django_apogee.models import AnneeUni, VersionEtape as VersionEtapeApogee, EtpGererCge, Etape
+from duck_inscription.models import SettingAnneeUni, SettingsEtape, Wish
+
+__author__ = 'paul'
+from django.core.management.base import BaseCommand
+APOGEE_CONNECTION = getattr(settings, 'APOGEE_CONNECTION', 'oracle')
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        pass
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ duck_svi/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+from django_apogee.models import Etape
+# Create your models here.
+
+
+class WishSvi(models.Model):
+    code_dossier = models.CharField(db_column='NumDossier', primary_key=True , max_length=10)
+    password = models.CharField(db_column='Password', null=False, default='1234')
+    etat = models.CharField(db_column='Etat', null=False, max_length=250)
+    gestionaire_etape = models.CharField(db_column='GestModif', null=False, max_length=6)
+    psycho = models.BooleanField(db_column='Psycho', null=False)
+    gestionaire_equiv = models.CharField(db_column='GestEquiv', null=True, max_length=6)
+    gestionaire_bourse = models.CharField(db_column='GestBourse', null=False, max_length=6)
+
+
+class GestToEtape(models.Model):
+    code_gestionaire = models.CharField(primary_key=True, max_length=6)
+    etape = models.ForeignKey(Etape, null=False)
+    nom = models.CharField(null=True, max_length=100)
+    prenom = models.CharField(null=True, max_length=200)
+
+
+
